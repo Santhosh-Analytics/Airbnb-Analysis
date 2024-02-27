455daf390667dfc4e9b6f82c9d73a4a340449588
Santhosh <sansuganyas@outlook.com>
2024-02-28 04:54:50 +0530

    Initial Commit

---
+++ jupyterlab.ps1
@@ -0,0 +1,6 @@
+Set-Alias jupyterlab 'E:\Technology\Python Project\Capstone\Airbnb\jupyterlab.ps1'
+# Change directory to the desired location
+Set-Location 'E:\Technology\Python Project\Capstone\Airbnb\'
+
+# Start Jupyter Lab
+Start-Process jupyter lab
